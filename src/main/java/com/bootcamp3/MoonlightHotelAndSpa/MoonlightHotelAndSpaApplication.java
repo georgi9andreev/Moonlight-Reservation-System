4336d2a9fb4b0621ca9bf080a6d529f34f76722d
StefanPenev <stfnpenev@gmail.com>
2022-06-08 23:20:41 +0300

    initial commit

---
+++ src/main/java/com/bootcamp3/MoonlightHotelAndSpa/MoonlightHotelAndSpaApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp3.MoonlightHotelAndSpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonlightHotelAndSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonlightHotelAndSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp3/MoonlightHotelAndSpa/MoonlightHotelAndSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp3.MoonlightHotelAndSpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonlightHotelAndSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
