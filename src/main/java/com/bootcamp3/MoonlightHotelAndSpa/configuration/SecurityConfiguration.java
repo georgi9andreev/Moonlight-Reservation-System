7333315d3967f8c6723dfba62dccd30b4e7b0afb
StefanPenev <stfnpenev@gmail.com>
2022-10-04 10:10:58 +0200

    Disabled security and authorization

--- src/main/java/com/bootcamp3/MoonlightHotelAndSpa/configuration/SecurityConfiguration.java
+++ src/main/java/com/bootcamp3/MoonlightHotelAndSpa/configuration/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.bootcamp3.MoonlightHotelAndSpa.configuration;
 
 import com.bootcamp3.MoonlightHotelAndSpa.exception.CustomHttp403ForbiddenEntryPoint;
 import com.bootcamp3.MoonlightHotelAndSpa.filter.CustomAccessDeniedHandler;
-import com.bootcamp3.MoonlightHotelAndSpa.filter.JwtTokenFilter;
 import com.bootcamp3.MoonlightHotelAndSpa.service.impl.UserServiceImpl;
 import com.bootcamp3.MoonlightHotelAndSpa.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +15,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static com.bootcamp3.MoonlightHotelAndSpa.constant.SecurityConstant.*;
+import static com.bootcamp3.MoonlightHotelAndSpa.constant.SecurityConstant.PROTECTED_URLS;
+import static com.bootcamp3.MoonlightHotelAndSpa.constant.SecurityConstant.PUBLIC_URLS;
 
 @Configuration
 @EnableWebSecurity
@@ -46,13 +45,15 @@ public class SecurityConfiguration {
         http
                 .authorizeRequests(authorize -> authorize
                         .antMatchers(PUBLIC_URLS).permitAll()
-                        .antMatchers(PROTECTED_URLS).hasAnyAuthority(ADMIN)
+                        //.antMatchers(PROTECTED_URLS).hasAnyAuthority(ADMIN)
+                        .antMatchers(PROTECTED_URLS).permitAll()
                         .antMatchers(HttpMethod.POST, "/users", "/rooms").permitAll()
-                        .antMatchers(HttpMethod.GET, "/users", "/rooms").hasAnyAuthority(ADMIN)
+                        .antMatchers(HttpMethod.GET, "/users", "/rooms").permitAll()
+                        //.antMatchers(HttpMethod.GET, "/users", "/rooms").hasAnyAuthority(ADMIN)
                         .anyRequest().denyAll())
-                .addFilterBefore(new JwtTokenFilter(jwtTokenUtil, userService), UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests()
-                .and()
+                //.addFilterBefore(new JwtTokenFilter(jwtTokenUtil, userService), UsernamePasswordAuthenticationFilter.class)
+                //.authorizeRequests()
+                //.and()
                 .exceptionHandling()
                 .accessDeniedHandler(accessDeniedHandler)
                 .authenticationEntryPoint(authenticationEntryPint)
